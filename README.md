2fb0940729bae4b5b68bd42cda51b21eb509a9b1
AndreiU <andrei.ulinici1@gmail.com>
2018-05-15 19:15:27 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# aaai-project
+
+
+ 
---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '1017711318281-r4n35776v0dsh5m9p4kncbles5s16597.apps.googleusercontent.com',
+            clientSecret: 'TtIndqOsbkJA41E5fQQke0CV'
+        },
+        mongodb: {
+            dbURI: 'mongodb://aaaiuser:passwordAAAI@ds117960.mlab.com:17960/aaai-project'
+        },
+        session: {
+            cookieKey: 'andreiucookiekeysessionaaai'
+        }
+};
